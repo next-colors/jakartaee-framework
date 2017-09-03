35bf89b7865e51cffb8c93aca2c1d410be98075b
hamana <doarat@gmail.com>
2017-09-03 22:17:57 +0900

    ライセンスコメントを修正

--- src/main/java/jp/co/nextcolors/framework/jdbc/pagination/Pager.java
+++ src/main/java/jp/co/nextcolors/framework/jdbc/pagination/Pager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 NEXT COLORS Co., Ltd. All Rights Reserved.
+ * (C) 2017 NEXT COLORS Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
