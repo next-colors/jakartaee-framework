8007b759a7f75898da72ce96d54d0ccafbf3f627
hamana <doarat@gmail.com>
2018-03-17 01:45:49 +0900

    リフレクションのためのライブラリを変更
    Reflections は Java 9 以降に対応していないため。

--- src/main/java/jp/co/nextcolors/framework/bean/util/BeanConverterUtil.java
+++ src/main/java/jp/co/nextcolors/framework/bean/util/BeanConverterUtil.java
@@ -17,23 +17,20 @@ package jp.co.nextcolors.framework.bean.util;
 
 import java.util.Set;
 
-import javax.servlet.ServletContext;
-
 import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.Converter;
 import org.apache.commons.lang3.reflect.ConstructorUtils;
 import org.apache.commons.lang3.tuple.Pair;
-import org.reflections.Reflections;
 
 import com.google.common.collect.ImmutableSet;
 
-import lombok.NonNull;
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
 import jp.co.nextcolors.framework.bean.annotation.BeanConverter;
-import jp.co.nextcolors.framework.servlet.util.ServletContextUtil;
 
 /**
  * Bean（JavaBeans）のコンバータに関するユーティリティです。
@@ -49,25 +46,19 @@ public class BeanConverterUtil
 	/**
 	 * {@link ConvertUtilsBean} に登録するコンバータとコンバータでの変換の対象となるクラスの対応表を返します。
 	 *
-	 * @param context
-	 * 			Servlet コンテキスト
 	 * @return {@link ConvertUtilsBean} に登録するコンバータとコンバータでの変換の対象となるクラスの対応表
 	 */
-	private static Set<Pair<Class<? extends Converter>, Class<?>>> getConversionRelations( @NonNull final ServletContext context )
+	private static Set<Pair<Class<? extends Converter>, Class<?>>> getConversionRelations()
 	{
 		ImmutableSet.Builder<Pair<Class<? extends Converter>, Class<?>>> builder = ImmutableSet.builder();
 
-		Reflections reflectionScanner = ServletContextUtil.getClassReflectionScanner( context );
-
-		Set<Class<? extends Converter>> converters = reflectionScanner.getSubTypesOf( Converter.class );
-
-		for ( Class<? extends Converter> converter : converters ) {
+		new FastClasspathScanner().matchClassesImplementing( Converter.class, converter -> {
 			if ( converter.isAnnotationPresent( BeanConverter.class ) ) {
 				Class<?> targetClass = converter.getAnnotation( BeanConverter.class ).forClass();
 
 				builder.add( Pair.of( converter, targetClass ) );
 			}
-		}
+		} ).scan();
 
 		return builder.build();
 	}
@@ -78,14 +69,12 @@ public class BeanConverterUtil
 	/**
 	 * {@link ConvertUtilsBean} にコンバータを登録します。
 	 *
-	 * @param context
-	 * 			Servlet コンテキスト
 	 * @see ConvertUtils#register(Converter, Class)
 	 */
 	@SneakyThrows(ReflectiveOperationException.class)
-	public static void registerConverters( @NonNull final ServletContext context )
+	public static void registerConverters()
 	{
-		Set<Pair<Class<? extends Converter>, Class<?>>> relations = getConversionRelations( context );
+		Set<Pair<Class<? extends Converter>, Class<?>>> relations = getConversionRelations();
 
 		for ( Pair<Class<? extends Converter>, Class<?>> relation : relations ) {
 			Converter converter = ConstructorUtils.invokeConstructor( relation.getLeft() );
