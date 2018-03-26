3c0d692ca484a3b0662ae81c372497475c3d05ab
hamana <doarat@gmail.com>
2018-03-26 19:52:03 +0900

    リファクタリング

--- src/main/java/jp/co/nextcolors/framework/bean/converter/EnumConverter.java
+++ src/main/java/jp/co/nextcolors/framework/bean/converter/EnumConverter.java
@@ -21,12 +21,12 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.beanutils.ConversionException;
 import org.apache.commons.beanutils.converters.AbstractConverter;
+import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.math.NumberUtils;
-
-import com.google.common.collect.ImmutableMap;
 
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
@@ -68,7 +68,7 @@ public abstract class EnumConverter<E extends Enum<E>> extends AbstractConverter
 	 */
 	private E convert( final int ordinal )
 	{
-		Map<Integer, E> constants = Arrays.stream( enumClass.getEnumConstants() ).collect( ImmutableMap.toImmutableMap( Enum::ordinal, Function.identity() ) );
+		Map<Integer, E> constants = Arrays.stream( enumClass.getEnumConstants() ).collect( Collectors.toMap( Enum::ordinal, Function.identity() ) );
 
 		if ( !constants.containsKey( ordinal ) ) {
 			throw new ConversionException( String.format( "序数 %s に %s は含まれていません。", constants.keySet(), ordinal ) );
@@ -88,7 +88,7 @@ public abstract class EnumConverter<E extends Enum<E>> extends AbstractConverter
 	 */
 	private E convert( @NonNull final String name )
 	{
-		Map<String, E> constants = Arrays.stream( enumClass.getEnumConstants() ).collect( ImmutableMap.toImmutableMap( Enum::name, Function.identity() ) );
+		Map<String, E> constants = EnumUtils.getEnumMap( enumClass );
 
 		if ( !constants.containsKey( name ) ) {
 			throw new ConversionException( String.format( "名前 %s に %s は含まれていません。", constants.keySet(), name ) );
@@ -126,11 +126,13 @@ public abstract class EnumConverter<E extends Enum<E>> extends AbstractConverter
 	@Override
 	protected <T> T convertToType( @NonNull final Class<T> type, @NonNull final Object value ) throws Throwable
 	{
-		if ( NumberUtils.isDigits( Objects.toString( value ) ) ) {
-			return type.cast( convert( NumberUtils.toInt( Objects.toString( value ) ) ) );
+		String key = Objects.toString( value );
+
+		if ( NumberUtils.isDigits( key ) ) {
+			return type.cast( convert( NumberUtils.toInt( key ) ) );
 		}
 
-		return type.cast( convert( Objects.toString( value ) ) );
+		return type.cast( convert( key ) );
 	}
 
 	/**
