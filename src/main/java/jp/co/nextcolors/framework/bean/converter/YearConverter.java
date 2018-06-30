5328f27d2fbec7bf7e3ef9a5026c624b684bee19
hamana <doarat@gmail.com>
2018-06-30 14:18:40 +0900

    Date And Time API（JSR 310）の対応クラスを追加

---
+++ src/main/java/jp/co/nextcolors/framework/bean/converter/YearConverter.java
@@ -0,0 +1,51 @@
+/*
+ * (C) 2017 NEXT COLORS Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.co.nextcolors.framework.bean.converter;
+
+import java.time.OffsetDateTime;
+import java.time.Year;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+
+import jp.co.nextcolors.framework.bean.annotation.BeanConverter;
+
+/**
+ * {@link Year} 型の年に変換するためのコンバータです。
+ *
+ * @author hamana
+ */
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@BeanConverter(forClass = Year.class)
+public class YearConverter extends JavaTimeConverter<Year>
+{
+	//-------------------------------------------------------------------------
+	//    Protected Methods
+	//-------------------------------------------------------------------------
+	/**
+	 * {@inheritDoc}
+	 *
+	 */
+	@Override
+	protected Year getDateTime( @NonNull final OffsetDateTime offsetDateTime )
+	{
+		return Year.from( offsetDateTime );
+	}
+}
