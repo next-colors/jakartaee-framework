f6212c783a0a084507388e36639c57e9f51bbf3b
Akihiro Hamana <doarat@gmail.com>
2022-09-19 10:40:04 +0900

    パッケージ名の変更

--- src/test/java/jp/co/nextcolors/framework/faces/converter/CodeEnumConverterTest.java
+++ src/test/java/jp/co/nextcolors/framework/faces/converter/CodeEnumConverterTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jp.co.nextcolors.framework.jsf.converter;
+package jp.co.nextcolors.framework.faces.converter;
 
 import static net.andreinc.mockneat.unit.objects.From.from;
 import static org.assertj.core.api.Assertions.assertThat;
